2bd6bc01ed80a2d2823fbd5f932fecb6a2d4a91d
Haris-AAI <Haris.Yousaf@automotive-ai.com>
2024-06-29 19:00:40 +0500

    Modified run.py script

--- Scripts/run.py
+++ Scripts/run.py
@@ -1,3 +1,5 @@
+import os
+import coloredlogs
 import yaml
 import logging
 import time
@@ -8,11 +10,25 @@ from preprocessor import Preprocessor
 
 
 def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    # Define the log format
+    log_format = '%(asctime)s - %(levelname)s - %(message)s'
+
+    # Define custom log colors
+    field_styles = {
+        'asctime': {'color': 'green'},
+        'levelname': {'color': 'yellow', 'bold': True},
+        'message': {'color': 'red'}
+    }
+
+    # Install coloredlogs with the custom format and field styles
+    coloredlogs.install(level='INFO', fmt=log_format, field_styles=field_styles)
+
+    logger = logging.getLogger(__name__)
+    return logger
 
 
 if __name__ == '__main__':
-    setup_logging()
+    logging = setup_logging()
 
     with open('../Config/external_settings.yaml', 'r') as external_settings:
         try:
@@ -23,6 +39,9 @@ if __name__ == '__main__':
 
     bag_file = ext_setts['bag_file']
     output_dir = ext_setts['output_dir']
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     run_dataExtractor = ext_setts['run_dataExtractor']
     run_preprocessor = ext_setts['run_preprocessor']
     run_kissICP = ext_setts['run_kissICP']
@@ -34,25 +53,25 @@ if __name__ == '__main__':
         dataExtractor = Extractor(bag_file, output_dir)
         dataExtractor.run()
         elapsed_time = time.time() - start_time
-        logging.info(f'Extractor module took {elapsed_time:.2f} seconds.')
+        logging.info(f'Extractor module took {elapsed_time:.2f} seconds.\n')
     if run_preprocessor:
         start_time = time.time()
         logging.info('Running Preprocessor module...')
         preProcessor = Preprocessor(output_dir)
         preProcessor.run()
         elapsed_time = time.time() - start_time
-        logging.info(f'Preprocessor module took {elapsed_time:.2f} seconds.')
+        logging.info(f'Preprocessor module took {elapsed_time:.2f} seconds.\n')
     if run_kissICP:
         start_time = time.time()
         logging.info('Running RunKissICP module...')
         kissicp = RunKissICP(output_dir)
         kissicp.run()
         elapsed_time = time.time() - start_time
-        logging.info(f'RunKissICP module took {elapsed_time:.2f} seconds.')
+        logging.info(f'RunKissICP module took {elapsed_time:.2f} seconds.\n')
     if run_trajectoryTransformer:
         start_time = time.time()
         logging.info('Running TrajectoryTransformer module...')
         trajectoryTransformer = TrajectoryTransformer(output_dir)
         trajectoryTransformer.run()
         elapsed_time = time.time() - start_time
-        logging.info(f'TrajectoryTransformer module took {elapsed_time:.2f} seconds.')
+        logging.info(f'TrajectoryTransformer module took {elapsed_time:.2f} seconds.\n')
